427e493b20fb8bab4add3097210dcdae85f9208d
Asgeir Frimannsson <asgeirf@gmail.com>
2009-04-15 07:49:23 +0000

    simplified roundtrip framework

--- jgettext/src/test/java/org/fedorahosted/tennera/jgettext/RoundTripTests.java
+++ jgettext/src/test/java/org/fedorahosted/tennera/jgettext/RoundTripTests.java
@@ -1,8 +1,13 @@
 package org.fedorahosted.tennera.jgettext;
 
-import static org.fedorahosted.tennera.jgettext.Assert.assertNoDiff;
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 
 import org.fedorahosted.tennera.jgettext.catalog.parse.ExtendedCatalogParser;
 import org.fedorahosted.tennera.jgettext.catalog.write.CatalogWriter;
@@ -14,21 +19,56 @@ public class RoundTripTests {
 	@Test
 	public void testRoundtrip1() throws Throwable{
 		File original = getResource("/roundtrip/sample.po");
-		File output = new File("test1.po.out");
-		roundtrip(original, output);
-		assertNoDiff(original, output);
+		testRoundTrip(original);
 	}
 
-	private void roundtrip(File original, File destination) throws Throwable{
+	private void testRoundTrip(String message, File f) throws Throwable{
+		String output = roundtrip(f);
+		String originalString = readToString(f); 
+		assertEquals(message, originalString, output);
+	}
+	
+	private void testRoundTrip(File f) throws Throwable{
+		testRoundTrip(null, f);
+	}
+	
+	private String roundtrip(File original) throws Throwable{
 		ExtendedCatalogParser parser = new ExtendedCatalogParser(original);
 		parser.catalog();
 		CatalogWriter writer = new CatalogWriter(parser.getCatalog());
-		writer.writeTo(destination);
+		StringWriter outputWriter = new StringWriter();
+		writer.writeTo(outputWriter);
+		outputWriter.flush();
+		return outputWriter.toString();
 	}
 	
 	private File getResource(String file){
 		return new File( getClass().getResource(file).getFile() );
 	}
 	
+    private static String readToString(File file) {
+
+    	BufferedReader reader = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+	    	reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+	 
+	        String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+            	reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        return sb.toString();
+    }
+
 	
 }
