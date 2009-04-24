ea6d995a519e79a446f7aa956005593a7c238ee9
Asgeir Frimannsson <asgeirf@gmail.com>
2009-04-24 04:09:42 +0000

    added test for fuzzy flag in Message

---
+++ jgettext/src/test/java/org/fedorahosted/tennera/jgettext/TestMessage.java
@@ -0,0 +1,27 @@
+package org.fedorahosted.tennera.jgettext;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestMessage {
+	
+	@Test
+	public void testIsFuzzyWhenEmptyMsgstr(){
+		Message msg = new Message();
+		msg.setMsgid("hello world!");
+		msg.setMsgstr("");
+		assertFalse(msg.isFuzzy());
+		
+		msg.markFuzzy();
+		assertFalse("Empty msgstr should not produce fuzzy", msg.isFuzzy());
+	}
+
+	public void testIsFuzzyWhenMarkedAsFuzzy(){
+		Message msg = new Message();
+		msg.setMsgid("hello world!");
+		msg.setMsgstr("hei verden");
+		msg.markFuzzy();
+		assertTrue(msg.isFuzzy());
+	}
+
+}
