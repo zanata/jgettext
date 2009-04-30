8477424dcae3845e249938b3514c3c8f4c33b11b
Asgeir Frimannsson <asgeirf@gmail.com>
2009-04-30 05:54:22 +0000

    added test case checking that flags are parsed in correctly

--- jgettext/src/test/java/org/fedorahosted/tennera/jgettext/TestMessage.java
+++ jgettext/src/test/java/org/fedorahosted/tennera/jgettext/TestMessage.java
@@ -1,5 +1,8 @@
 package org.fedorahosted.tennera.jgettext;
 
+import java.io.File;
+
+import org.fedorahosted.tennera.jgettext.catalog.parse.MessageStreamParser;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -27,5 +30,19 @@ public class TestMessage {
 		msg.setFuzzy(false);
 		assertFalse(msg.isFuzzy());
 	}
+	
+	
+	@Test
+	public void testFlagsAreWorkingAsExpected() throws Throwable{
+		File poFile = getResource("/flags.po");
+		PoParser parser = new PoParser();
+		Message msg = parser.parseMessage(poFile);
+		assertTrue(msg.isFuzzy());
+		assertTrue(msg.getFormats().contains("no-c-format"));
+	}
+	
+	private File getResource(String file){
+		return new File( getClass().getResource(file).getFile() );
+	}	
 
 }
